de3d6024b13147e69fa65d98c44d051684afc3fb
pushpithaDilhan <pushpitha.14@cse,mrt.ac.lk>
2016-07-24 12:53:22 +0530

    initial commit

---
+++ app/src/main/java/com/example/pushpitha/constructionhelper/Choose_the_Shape.java
@@ -0,0 +1,13 @@
+package com.example.pushpitha.constructionhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Choose_the_Shape extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_the__shape);
+    }
+}
