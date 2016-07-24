eb490cbd9acab5833a0c3d6f93da0b83528555f8
pushpithaDilhan <pushpitha.14@cse,mrt.ac.lk>
2016-07-24 20:43:49 +0530

    Completed circle part

--- app/src/main/java/com/example/pushpitha/constructionhelper/Choose_the_Shape.java
+++ app/src/main/java/com/example/pushpitha/constructionhelper/Choose_the_Shape.java
@@ -1,13 +1,42 @@
 package com.example.pushpitha.constructionhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Choose_the_Shape extends AppCompatActivity {
+
+    ImageButton circle;
+    ImageButton rectangle;
+    ImageButton parallelogram;
+    ImageButton bulletshape;
+    ImageButton oval;
+    ImageButton houseshape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_the__shape);
+        circle = (ImageButton) findViewById(R.id.circle);
+        rectangle = (ImageButton) findViewById(R.id.rectangle);
+        parallelogram = (ImageButton) findViewById(R.id.parallelogram);
+        bulletshape = (ImageButton) findViewById(R.id.bulletshape);
+        oval = (ImageButton) findViewById(R.id.oval);
+        houseshape = (ImageButton) findViewById(R.id.houseshape);
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Clicked");
+                if(v.getId()==R.id.circle){
+                    Intent circle_next = new Intent(getApplicationContext(),circle.class);
+                    startActivity(circle_next);
+                }
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/pushpitha/constructionhelper/circle.java
@@ -0,0 +1,91 @@
+package com.example.pushpitha.constructionhelper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class circle extends AppCompatActivity {
+
+    TextView areaAnswer;
+    TextView volAnswer ;
+    float radius;
+    float thick;
+    float area_answer;
+    float volume;
+    EditText r;
+    EditText t;
+    Boolean hasFuture = false;                           // to indicate whether there are future events
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle);
+        r = (EditText) findViewById(R.id.txt_radius);
+        t = (EditText) findViewById(R.id.txt_thick);
+        Button area = ( Button) findViewById(R.id.button_area);
+        Button vol = ( Button) findViewById(R.id.button_volume);
+        Button next = ( Button) findViewById(R.id.button_next);
+        areaAnswer = (TextView) findViewById(R.id.area_ans);
+        volAnswer = (TextView) findViewById(R.id.volume_ans);
+
+        //when area button is pressed
+        area.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    radius = Float.parseFloat(r.getText().toString().trim());
+                    System.out.println(radius);
+                    area_answer = (float) Math.PI*radius*radius ;
+                    areaAnswer.setText(area_answer+"");
+                    volAnswer.setText("");
+                }catch(Exception e){
+                    Toast.makeText(getApplicationContext() ,"R value is not acceptable",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //when volume button is pressed
+        vol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String thick_s = t.getText().toString();
+                if(thick_s.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Thickness is needed.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    try{
+                        thick = Float.parseFloat(thick_s);
+                        radius = Float.parseFloat(r.getText().toString().trim());
+                        area_answer = (float) Math.PI*radius*radius;
+                        volume = thick*area_answer ;
+                        volAnswer.setText(volume+"");
+                        areaAnswer.setText(area_answer+"");
+                        hasFuture = true;
+                    }catch(Exception e){
+                        Toast.makeText(getApplicationContext(),"Thickness or R is not acceptable.",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        //when next button is pressed
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(hasFuture){
+                    Intent category = new Intent(getApplicationContext(),Category.class);
+                    startActivity(category);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Volume is needed.",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+}
